55d1f5c3ffdc8741367137f581c403c9fd0a5110
mbrylka <maciejbrylka94@gmail.com>
2021-07-10 14:26:38 +0200

    Rozszerzenie TaskModel

--- app/src/main/java/com/example/taskapp/models/TaskModel.java
+++ app/src/main/java/com/example/taskapp/models/TaskModel.java
@@ -6,6 +6,15 @@ public class TaskModel {
     public String taskName;
     public String taskDate;
     public String taskTime;
+    public int notificationType;
+    public boolean notify;
+
+    private enum NotificationTypeEnum {
+        MIN15,
+        MIN30,
+        HOUR1,
+        DAY1;
+    }
 
     public TaskModel(String taskName, String taskDate, String taskTime) {
         this.taskName = taskName;
